5c466ce834d9f9314c0e83cbd6e387dbc768c05a
billylam <billy.lam@crypto.com>
2023-06-13 18:09:34 +0800

    created a tour guide and readme file and all done

--- settings.py
+++ settings.py
@@ -6,7 +6,6 @@ if os.path.exists(os.path.dirname(__file__) + "/yml/config.yml"):
     env = env.lower()
 
     CONFIG = yaml.safe_load(open(os.path.dirname(__file__) + "/yml/config.yml", 'r'))[env]
-    # TEST_DATA = yaml.safe_load(open(os.path.dirname(__file__) + "/yml/{}.yml".format(env)))
 
     if not CONFIG:
         raise ValueError("Invalid Environment")
--- steps/nine_days_forecast.py
+++ steps/nine_days_forecast.py
@@ -1,5 +1,4 @@
 from time import sleep
-
 from page import Pages
 from pytest_bdd import given, when, then
 from selenium.common.exceptions import NoSuchElementException
